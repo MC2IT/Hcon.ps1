7d6a6c9866393a1b05b21df04b08917077119fe8
Cédric Belin <cedx@outlook.com>
2026-09-05 19:18:43 +0200

    Add a code sample [skip ci]

---
+++ Examples/ConvertFrom-Hcon.ps1
@@ -0,0 +1,9 @@
+﻿<#
+.SYNOPSIS
+	Converts the specified HCON-formatted string to a hash table.
+#>
+using module Mc2it.Hcon
+
+$hcon = "innerHTML swap:200ms settle:100ms"
+$hashtable = $hcon | ConvertFrom-Hcon
+Write-Output $hashtable
