ba64a8774b91b9a01e9dc6298e4c536fed9badde
Cédric Belin <cedx@outlook.com>
2026-09-05 13:18:15 +0200

    Add the `Debug` and `Invoke` scripts

---
+++ Debug.ps1
@@ -0,0 +1,8 @@
+#!/usr/bin/env pwsh
+using module ./Lcov.psd1
+
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
+
+try { <# Insert the command to be debugged here. #> }
+catch { Write-Error "$_`n$($_.ScriptStackTrace)" }
---
+++ Invoke.ps1
@@ -0,0 +1,18 @@
+#!/usr/bin/env pwsh
+param (
+	# The name of the task to invoke.
+	[Parameter(Position = 1)]
+	[ArgumentCompleter({
+		param ([string] $commandName, [string] $parameterName, [string] $wordToComplete)
+		(Get-Item "$PSScriptRoot/Scripts/$wordToComplete*.ps1").BaseName
+	})]
+	[ValidateScript({ Test-Path "$PSScriptRoot/Scripts/$_.ps1" -PathType Leaf }, ErrorMessage = "The specified command does not exist.")]
+	[string] $Command = "Default",
+
+	# Value indicating whether to enable the release configuration.
+	[switch] $Release
+)
+
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
+& "$PSScriptRoot/Scripts/$Command.ps1"
